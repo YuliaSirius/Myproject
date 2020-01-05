95ed85de4fb9b3ce297ec71f26dae91e68933ec4
Yulia Miasnikova <miasnikovasirius@gmail.com>
2020-01-05 11:46:55 +0300

    05.01.20

--- js/js.js
+++ js/js.js
@@ -5,23 +5,17 @@ $(document).ready(function() {
     asNavFor: ".slider2",
     dots: true,
     arrows: false
-    // infinite: false,
-  });
+     });
 });
-
 $(document).ready(function() {
   $(".slider2").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     asNavFor: ".slider1",
-    // centerMode: false,
-    // centerPadding: '156px',
-    // infinite: false,
-    prevArrow: false
+       prevArrow: false
   });
 });
-
 $(document).ready(function() {
   $(".slider-steps").slick({
     slidesToShow: 1,
@@ -30,3 +24,16 @@ $(document).ready(function() {
     arrows: false,
   });
 });
+function vidplay() {
+  let video = document.getElementById("Video1");
+  let button = document.getElementById("play");
+  if (video.paused) {
+     video.play();
+     button.style.opacity="0";
+        //  button.textContent = "5";
+  } else {
+     video.pause();
+     button.style.opacity="1";
+  //    button.textContent = "";
+  }
+}
