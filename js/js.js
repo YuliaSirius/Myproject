f6140c1865b4deea9a9cd3c27cd093ead764d6a7
Yulia Miasnikova <miasnikovasirius@gmail.com>
2020-01-09 18:15:08 +0300

    09.01.20

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+// Sliders
 $(document).ready(function() {
   $(".slider1").slick({
     slidesToShow: 1,
@@ -6,7 +7,7 @@ $(document).ready(function() {
     dots: true,
     arrows: true,
     swipeToSlide: true,
-    infinite: false,
+    infinite: false
   });
 });
 $(document).ready(function() {
@@ -16,7 +17,7 @@ $(document).ready(function() {
     arrows: true,
     swipeToSlide: true,
     asNavFor: ".slider1",
-    infinite: false,
+    infinite: false
     //  prevArrow: false
   });
 });
@@ -27,19 +28,98 @@ $(document).ready(function() {
     dots: true,
     swipeToSlide: true,
     arrows: true,
-    infinite: false,
+    infinite: false
   });
 });
-function vidplay() {
-  let video = document.getElementById("Video1");
-  let button = document.getElementById("play");
+// Buttons for video
+let restart = document.querySelector(".restart");
+let rew = document.querySelector(".rew");
+let play = document.querySelector(".play");
+let stop = document.querySelector(".stop");
+let fastfwd = document.querySelector(".fastfwd");
+let button = document.querySelector(".btnvideo");
+let video = document.querySelector(".video");
+let btns = document.querySelector(".btns");
+
+button.addEventListener("click", function() {
   if (video.paused) {
     video.play();
     button.style.opacity = "0";
-    //  button.textContent = "5";
+    btns.style.zIndex = "0";
   } else {
     video.pause();
     button.style.opacity = "1";
-    //    button.textContent = "";
   }
-}
+});
+play.addEventListener("click", function() {
+  if (video.paused) {
+    video.play();
+    button.style.opacity = "0";
+    play.style.display = "none";
+    stop.style.display = "inline";
+  } else {
+    button.style.opacity = "1";
+  }
+});
+stop.addEventListener("click", function() {
+  video.pause();
+  play.style.display = "inline";
+  stop.style.display = "none";
+});
+restart.addEventListener("click", function() {
+  video.currentTime = 0;
+});
+rew.addEventListener("click", function() {
+  video.currentTime += -10;
+});
+fastfwd.addEventListener("click", function() {
+  video.currentTime += 10;
+});
+// Tabs
+// let tab = function() {
+//   let tabNav = document.querySelectorAll(".tab"),
+//     tabContent = document.querySelectorAll(".tab-content"),
+//     tabName;
+
+//   tabNav.forEach(item => {
+//     item.addEventListener("click", selectTabNav);
+//   });
+
+//   function selectTabNav() {
+//     tabNav.forEach(item => {
+//       item.classList.remove("slick-active");
+//     });
+//     this.classList.add("slick-active");
+//     tabName = this.getAttribute("data-tab-name");
+//     selectTabContent(tabName);
+//   }
+
+//   function selectTabContent(tabName) {
+//     tabContent.forEach(item => {
+//       item.classList.contains(tabName)
+//         ? item.classList.add("slick-active")
+//         : item.classList.remove("slick-active");
+//     });
+//   }
+// };
+// tab();
+
+// window.onload = function() {
+//   document.querySelector(".tab-header").addEventListener("click", fTabs);
+//   function fTabs(event) {
+//        if (event.target.className == "tab-h") {
+//       let dataTab = event.target.getAttribute("data-tab");
+//       let slickAct = document.querySelector(".slick-active");
+//       console.log(slickAct);
+
+//       // let tabBody = document.querySelectorAll(".tab-b");
+//       // for (let i = 0; i < tabBody.length; i++) {
+//         if (dataTab == slickAct) {
+//           tabBody[i].style.display = "block";
+//         } else {
+//           tabBody[i].style.display = "none";
+//         }
+//       // }
+//     }
+//   }
+// };
