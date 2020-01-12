865abdb76d6e7b970df3570e7a4ae9ffe6efe379
Yulia Miasnikova <miasnikovasirius@gmail.com>
2020-01-12 17:25:56 +0300

    12.01.20

--- js/js.js
+++ js/js.js
@@ -18,7 +18,6 @@ $(document).ready(function() {
     swipeToSlide: true,
     asNavFor: ".slider1",
     infinite: false
-    //  prevArrow: false
   });
 });
 $(document).ready(function() {
@@ -75,6 +74,7 @@ rew.addEventListener("click", function() {
 fastfwd.addEventListener("click", function() {
   video.currentTime += 10;
 });
+
 // Tabs
 // let tab = function() {
 //   let tabNav = document.querySelectorAll(".tab"),
@@ -104,22 +104,79 @@ fastfwd.addEventListener("click", function() {
 // };
 // tab();
 
-// window.onload = function() {
-//   document.querySelector(".tab-header").addEventListener("click", fTabs);
-//   function fTabs(event) {
-//        if (event.target.className == "tab-h") {
-//       let dataTab = event.target.getAttribute("data-tab");
-//       let slickAct = document.querySelector(".slick-active");
-//       console.log(slickAct);
+// document.querySelector(".tab-header").addEventListener("click", fTabs);
 
-//       // let tabBody = document.querySelectorAll(".tab-b");
-//       // for (let i = 0; i < tabBody.length; i++) {
-//         if (dataTab == slickAct) {
-//           tabBody[i].style.display = "block";
-//         } else {
-//           tabBody[i].style.display = "none";
-//         }
-//       // }
-//     }
+// function fTabs(event) {
+//      if (event.target.className == "tab-h") {
+//     let dataTab = event.target.getAttribute("data-tab");
+//     let slickAct = document.querySelector(".slick-active");
+//     console.log(slickAct);
+
+//     // let tabBody = document.querySelectorAll(".tab-b");
+//     // for (let i = 0; i < tabBody.length; i++) {
+//       if (dataTab == slickAct) {
+//         tabBody[i].style.display = "block";
+//       } else {
+//         tabBody[i].style.display = "none";
+//       }
+//     // }
 //   }
 // };
+
+// function openCity(evt, cityName) {
+//   let i, tabcontent, tablinks;
+
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   // for (i = 0; i < tabcontent.length; i++) {
+//   //   tabcontent[i].style.opacity = "0";
+//   // }
+
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].className = tabcontent[i].className.replace(
+//       " slick-current slick-active",
+//       ""
+//     );
+//   }
+
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+
+//   // document.getElementById(cityName).className = " slick-current slick-active";
+//   // evt.currentTarget.className += " active";
+// }
+
+
+// Tab for projects
+
+
+// function openCity(evt, cityName) {
+//   let i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   tablinks = document.getElementsByClassName("tablinks");
+
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   evt.currentTarget.className += " active";
+//   document.getElementById(cityName).style.display = "block";
+// }
+// Tab for steps
+function tab(evt, steps) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(steps).style.display = "block";
+  evt.currentTarget.className += " active";
+}
